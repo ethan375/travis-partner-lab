6bd11547d4300f47d1f52a1a89307415737c1dd1
ethan375 <ethanebel1@gmail.com>
2018-02-07 19:25:08 -0600

    finished all crud routes

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -3,47 +3,28 @@ class ApplicationController < Sinatra::Base
   require 'bundler' 
   Bundler.require()
 
-
   register Sinatra::CrossOrigin
 
   configure do 
     enable :cross_origin
   end 
 
-
-
-  register Sinatra::CrossOrigin
-
-  configure do
-    enable :cross_origin
-  end
-
-
   set :allow_origin, :any
-  set :allow_methods, [:get, :post, :delete, :options, :put]
-  set :allow_credentials, true
+  set :allow_methods, [:get, :post, :options, :put, :delete]
+  set :allow_credentials, true 
 
-  ActiveRecord::Base.establish_connection(
+
+    ActiveRecord::Base.establish_connection(
     :adapter => 'postgresql',
     :database => 'chakra'
-  )
+    )
 
-  options "*" do 
-
-    response.headers["Allow"] ="HEAD, GET, PUT, POST, DELETE, OPTIONS"
+      options "*" do 
+    response.headers["Allow"] ="HEAD, GET, PUT, POST, DELTE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] ="X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-  end 
-
-  get '/' do
-
-    # p "options request sent"
-    response.headers["Allow"] ="HEAD, GET, PUT, POST, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Origin"] = '*'
-    response.headers["Access-Control-Allow-Headers"] ="X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-  end 
+  end
 
     get '/' do 
-
     redirect '/chakras'
   end 
 
--- controllers/ChakraController.rb
+++ controllers/ChakraController.rb
@@ -19,11 +19,40 @@ class ChakraController < ApplicationController
 
   get '/:id' do 
     @chakra = Chakra.find params[:id]
-    @chakra
+    @chakra.name = params[:name]
+    @chakra.color = params[:color]
+    @chakra.size = params[:size]
+    @chakra.size
+    resp = {
+      status: {
+        success: true
+      }
+    }
+    resp.to_json
   end 
 
   delete '/:id' do 
     @chakra = Chakra.find params[:id]
+    @chakra.delete
+    resp = {
+      status:{
+        deleted: true,
+        message: "you deleted #{params[:id]}"
+      }
+    }
+    resp.to_json
+  end 
+
+  put '/:id' do 
+    payload_body = request.body.read
+    payload = JSON.parse(payload_body).symbolize_keys
+
+    @chakra = Chakra.find params[:id]
+    @chakra.name = payload[:name]
+    @chakra.color = payload[:color]
+    @chakra.size = payload[:size]
+    @chakra.save
+  end 
 
 
 end 
